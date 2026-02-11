80d02eb97d057c867fd9ae2560dbeee2706cc156
AliWanton <hezareh22@gmail.com>
2026-02-11 15:16:03 +0100

    individuelt api test souderbroderna

---
+++ tests/test_souderbroders.py
@@ -0,0 +1,11 @@
+import pytest, requests
+
+baseUrl = "https://kzmcpfklrqymzazaxlmv.supabase.co/functions/v1/partner-loan-api"
+API_KEY = "AliR,75833cf59bc93ea84745f49cb713319f680b03053cc365be0fa116d8883d1b66,4c13d26460527b2c6e9ecc3370a65330cf3a431b97dc9ad4724a88a00ba4d76d"
+
+
+# Minst ett "tekniskt" test mha requests-biblioteket, där headers, HTTP statuskod mm valideras
+
+def test_(pet):
+
+#jag ska testa att kopla till apier och att den ger rätt statuskod och att den inte är tom
